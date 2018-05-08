fe3c0024975027080d3408085e90312c87ec91ee
Pedro <pedropacheco92@outlook.com>
2018-05-08 13:07:58 -0300

    wip

--- src/main/java/com/tantum/app/tantum/TantumControllerAdvice.java
+++
@@ -1,8 +0,0 @@
-package com.tantum.app.tantum;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice(basePackages = { "com.tantum" })
-public class TantumControllerAdvice {
-
-}
