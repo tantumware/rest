03c8af18dee35ee619867eec7e7c87aad8e03bc3
Pedro <pedropacheco92@outlook.com>
2018-05-22 17:49:38 -0300

    testes

--- src/test/java/com/tantum/app/tantum/AlgorirmoTest.java
+++ src/test/java/com/tantum/app/tantum/AlgorirmoTest.java
@@ -2,17 +2,26 @@ package com.tantum.app.tantum;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.google.gson.Gson;
+import com.tantum.app.tantum.algoritmo.Algoritmo;
+import com.tantum.app.tantum.helper.Helper;
 import com.tantum.app.tantum.models.Constraints;
+import com.tantum.app.tantum.models.Course;
+import com.tantum.app.tantum.models.History;
+import com.tantum.app.tantum.models.NextSemestersDTO;
 import com.tantum.app.tantum.models.Periodo;
+import com.tantum.app.tantum.models.SemesterHistory;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,8 +29,37 @@ public class AlgorirmoTest {
 
 	private static Constraints constraints = new Constraints();
 
+	private static Algoritmo alg;
+
+	private static SemesterHistory semesterHistory;
+
 	@BeforeClass
 	public static void before() {
+		constraints.setPeriods(Arrays.asList(Periodo.values()));
+		constraints.setCreditMax(30);
+		constraints.setCreditMin(0);
+		constraints.setEquivalent(true);
+		constraints.setSubjectsWanted(Arrays.asList());
+		constraints.setSubjectsNotWanted(Arrays.asList());
+
+		String c = Helper.course_test;
+		String h = Helper.class_history_test;
+
+		Gson g = new Gson();
+		Course curso = g.fromJson(c, Course.class);
+		History history = g.fromJson(h, History.class);
+
+		semesterHistory = history.getSemesters().stream().reduce((x, y) -> {
+			x.getSubjects().addAll(y.getSubjects());
+			return x;
+		}).orElse(new SemesterHistory("", Arrays.asList()));
+
+		alg = new Algoritmo(curso);
+		alg.rankDisciplinas();
+	}
+
+	@Before
+	public void beforeEach() {
 		constraints.setPeriods(Arrays.asList(Periodo.values()));
 		constraints.setCreditMax(30);
 		constraints.setCreditMin(0);
@@ -33,7 +71,33 @@ public class AlgorirmoTest {
 	@Test
 	public void testConstraints() {
 		assertNotNull(constraints);
+		assertNotNull(constraints.getPeriods());
+		assertNotNull(constraints.getCreditMin());
+		assertNotNull(constraints.getCreditMax());
+		assertNotNull(constraints.getSubjectsNotWanted());
+		assertNotNull(constraints.getSubjectsWanted());
+		assertNotNull(constraints.isEquivalent());
+
 		assertFalse(constraints.getPeriods().isEmpty());
+		assertTrue(constraints.getCreditMax() > 0);
+	}
+
+	@Test
+	public void testAllPeriods() {
+		// given
+
+		// when
+		NextSemestersDTO result = alg.calculateSemesters(constraints, semesterHistory.getSubjects());
+
+		assertFalse(result.getNextSemesters().isEmpty());
+
+	}
+
+	@Test
+	public void testNoPeriods() {
+		// given
+		constraints.setPeriods(Arrays.asList());
+
 	}
 
 }
